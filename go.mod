fbc8f59203f9a97a1a4d676323642101cedc5823
AOreshin <hummr65@gmail.com>
2022-07-17 00:16:09 +0300

    Record audio

--- go.mod
+++ go.mod
@@ -11,6 +11,9 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/youpy/go-riff v0.1.0 // indirect
+	github.com/youpy/go-wav v0.3.2 // indirect
+	github.com/zaf/g711 v0.0.0-20220109202201-cf0017bf0359 // indirect
 	golang.org/x/sys v0.0.0-20220708085239-5a0f0661e09d // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"time"
 
 	"github.com/eiannone/keyboard"
 	"github.com/yanel/go-rtaudio/src/contrib/go/rtaudio"
+	"github.com/youpy/go-wav"
 )
 
 const (
-	AUDIO_BUFFER          = 16
-	DEFAULT_DURATION_TICS = 600000
+	AUDIO_BUFFER                 = 16
+	DEFAULT_DURATION_TICS        = 600000
+	numSamples            uint32 = math.MaxUint32
+	numChannels           uint16 = 2
+	sampleRate            uint32 = 44100
+	bitsPerSample         uint16 = 32
 )
 
 func main() {
+	outfile, err := os.Create(time.Now().Format("2006-02-01 15-04-05") + ".wav")
+	if err != nil {
+		panic(err)
+	}
+	defer outfile.Close()
+
+	writer := wav.NewWriter(outfile, numSamples, numChannels, sampleRate, bitsPerSample)
+
 	audio, err := rtaudio.Create(rtaudio.APIUnspecified)
 	if err != nil {
 		panic(err)
@@ -37,7 +52,15 @@ func main() {
 	cb := func(out, in rtaudio.Buffer, dur time.Duration, status rtaudio.StreamStatus) int {
 		samples := out.Float32()
 		for i := 0; i < len(samples)/2; i++ {
-			samples[i*2], samples[i*2+1] = stringSamples(strings)
+			l, r := stringSamples(strings)
+			samples[i*2], samples[i*2+1] = l, r
+			s := []wav.Sample{
+				{Values: [2]int{int(l * math.MaxInt32), int(r * math.MaxInt32)}},
+			}
+			err = writer.WriteSamples(s)
+			if err != nil {
+				panic(err)
+			}
 		}
 		return 0
 	}
